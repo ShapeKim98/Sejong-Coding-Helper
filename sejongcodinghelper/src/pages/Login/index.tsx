34feb2b7fa8c51e4cff5d6c10d70b45c0cddcf58
Seongmin Kim <44383895+fing9@users.noreply.github.com>
2023-12-06 21:05:53 +0900

    Revert "Feat: 회원가입 기능 개발"

--- sejongcodinghelper/src/pages/Login/index.tsx
+++ sejongcodinghelper/src/pages/Login/index.tsx
@@ -1,70 +1,65 @@
 import React, { useState, useCallback } from 'react';
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux'
 import { setUser } from '../../redux/User';
 import { userLogin } from '../../api/User/User';
-import { onSilentRefresh, setRefreshTokenToCookie } from '../../api/Auth/Auth';
+import { 
+    onSilentRefresh,
+    setRefreshTokenToCookie
+} from '../../api/Auth/Auth';
 import { useNavigate } from 'react-router-dom';
 import VStack from '../../components/VStack';
-import {
-  Title,
-  TextFieldTitle,
-  TextFieldStyle,
-  LoginButton,
-  JoinButton,
-} from './style';
+import { 
+    Title,
+    TextFieldTitle,
+    TextFieldStyle,
+    LoginButton
+ } from './style';
 import { ReactComponent as SejongLogo } from '../../assets/Image/SejongLogo.svg';
 
 const JWT_EXPIRY_TIME = 60 * 1000;
 
 function TextField(info: {
-  title: string;
-  text: string;
-  isPassword: boolean;
-  onChange: (e: {
-    target: {
-      value: React.SetStateAction<string>;
-    };
-  }) => void;
+    title: string,
+    text: string,
+    isPassword: boolean
+    onChange: (e: {
+        target: {
+            value: React.SetStateAction<string>;
+        };
+    }) => void
 }): React.ReactElement {
-  return (
-    <VStack style={{ marginBottom: '52px' }}>
-      <TextFieldTitle style={{ marginBottom: '16px' }}>
-        {info.title}
-      </TextFieldTitle>
-      <TextFieldStyle
-        onChange={info.onChange}
-        value={info.text}
-        type={info.isPassword ? 'password' : ''}
-        placeholder={(info.isPassword ? '' : '백준 ') + info.title}
-      />
-    </VStack>
-  );
+    return (
+        <VStack style={{marginBottom: '52px'}}>
+            <TextFieldTitle style={{marginBottom: '16px'}}>
+                {info.title}
+            </TextFieldTitle>
+            <TextFieldStyle
+                onChange={info.onChange} 
+                value={info.text}
+                type={info.isPassword ? 'password' : ''} 
+                placeholder={(info.isPassword ? '' : '백준 ') + info.title} />
+        </VStack>
+    )
 }
 
 function Login(): React.ReactElement {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
   const navigate = useNavigate();
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
   const [loginError, setLoginError] = useState(false);
   const [errorMsg, setErrorMsg] = useState('');
 
-  const changeId = useCallback(
-    (e: { target: { value: React.SetStateAction<string> } }) => {
-      setId(e.target.value);
-    },
-    []
-  );
-  const changePassword = useCallback(
-    (e: { target: { value: React.SetStateAction<string> } }) => {
-      setPassword(e.target.value);
-    },
-    []
-  );
+  const changeId = useCallback((e: { target: { value: React.SetStateAction<string>; }; }) => {
+    setId(e.target.value);
+  }, []);
+  const changePassword = useCallback((e: { target: { value: React.SetStateAction<string>; }; }) => {
+    setPassword(e.target.value);
+  }, []);
 
   const loginProc = useCallback(
-    (e: { preventDefault: () => void }) => {
+    (e: { preventDefault: () => void; }) => {
       e.preventDefault();
       if (!id || !id.trim()) {
         setLoginError(true);
@@ -87,7 +82,7 @@ function Login(): React.ReactElement {
           const { accessToken, refreshToken } = data.jwt;
           axios.defaults.headers.common['Access_Token'] = accessToken;
           dispatch(
-            setUser({ bojHandle: data.bojHandle, isAdmin: data.manager })
+            setUser({ bojHandle: data.bojHandle, isAdmin: data.manager }),
           );
           // 리프레쉬 토큰 저장
           setRefreshTokenToCookie(refreshToken);
@@ -102,63 +97,38 @@ function Login(): React.ReactElement {
           setErrorMsg('아이디 또는 비밀번호를 다시 확인해주세요.');
         });
     },
-    [id, password]
+    [id, password],
   );
 
-  return (
-    <VStack
-      style={{
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <SejongLogo style={{ marginTop: '100px' }} />
+    return (
+        <VStack style={{
+          alignItems: 'center', 
+          justifyContent: 'center'
+          }}>
+          <SejongLogo style={{marginTop: '100px'}} />
 
-      <Title style={{ marginBottom: '76px' }}>Univps</Title>
+          <Title style={{marginBottom: '76px'}}>
+              Univps
+          </Title>
 
-      <form
-        onSubmit={loginProc}
-        style={{
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <VStack
-          style={{
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <TextField
-            title="아이디"
-            text={id}
-            isPassword={false}
-            onChange={changeId}
-          />
-          <TextField
-            title="비밀번호"
-            text={password}
-            isPassword={true}
-            onChange={changePassword}
-          />
-          <LoginButton
-            style={{
-              marginTop: '66px',
-            }}
-          >
-            로그인
-          </LoginButton>
-          <JoinButton
-            onClick={() => {
-              navigate('/join');
-            }}
-          >
-            회원가입
-          </JoinButton>
+          <form onSubmit={loginProc} style={{
+            alignItems: 'center', 
+            justifyContent: 'center'
+          }}>
+            <VStack style={{
+              alignItems: 'center', 
+              justifyContent: 'center'
+            }}>
+              <TextField title='아이디' text={id} isPassword={false} onChange={changeId} />
+              <TextField title='비밀번호' text={password} isPassword={true} onChange={changePassword} />
+              <LoginButton style={{
+                marginTop: '66px'}}>
+                  로그인
+              </LoginButton>
+            </VStack>
+          </form>
         </VStack>
-      </form>
-    </VStack>
-  );
+    )
 }
 
-export default Login;
+export default Login
